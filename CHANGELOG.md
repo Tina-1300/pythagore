156d60cbefba0a816b1e86bcb2c5fb3b2cd2aaa4
Tina-1300 <tina.xytrfgthuji1348@gmail.com>
2026-02-20 07:42:44 +0100

    fix: use math.isclose for robust float comparaison (v1.3.1)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,45 @@
 # 📜 Changelog
 
-## [1.0.0] - 2025-05-02
-### Add
-- added is_rectangle, adjacent_side, hypotenus methods
+All notable changes to this project will be documented in this file.
 
+The format is based on Keep a Changelog
+and this project adheres to Semantic Versioning (SemVer).
+
+---
+
+## [1.3.1] - 2026-02-20
+### Fixed
+- Replaced direct float comparison in `is_rectangle` with `math.isclose`
+- Improved numerical stability and prevented floating-point precision issues
+
+---
+
+## [1.3.0] - 2025-09-06 
+### Added
+- `current_version()` method
+- `creator()` method
+-  Improved class representation
+
+---
+
+## [1.2.0] - 2025-08-26
+### Update
+- unit test
+
+---
+
+## [1.1.0] - 2025-08-26
+### Added
+- unit test
+- CI/CD
+
+---
+
+## [1.0.0] - 2025-05-02
+
+### Added
+-  Initial release
+- `Pythagore` class
+- `is_rectangle()` method
+- `adjacent_side()` method
+- `hypotenus()` method
--- pythagore/pythagore.py
+++ pythagore/pythagore.py
@@ -1,22 +1,15 @@
 import math
 
 class Pythagore:
-
     """
     The Pythagoras class makes it easier to calculate the Pythagorean theorem.
     """
 
     # parameter OP the largest side, the other two parameters are the other values ​​of the rectangle
     @staticmethod
-    def is_rectangle(hypotenuse, cote_a, cote_b):
-        result_op = math.pow(hypotenuse, 2)
-        pn = math.pow(cote_a, 2)
-        no = math.pow(cote_b, 2)
-        result = pn + no
-        if result_op != result:
-            return False
-        return True
-        
+    def is_rectangle(hypotenuse, side_a, side_b):
+        return math.isclose(hypotenuse**2, side_a**2 + side_b**2, rel_tol=1e-9)
+
     # hypotenus parameter, one side to find the missing side
     @staticmethod 
     def adjacent_side(hypotenuse, other_side):
@@ -24,7 +17,20 @@ class Pythagore:
 
     # the two sides of the triangle to find the hypotenus
     @staticmethod
-    def hypotenus(cote_a, cote_b):
-        return math.sqrt(math.pow(cote_a, 2) + math.pow(cote_b, 2))
+    def hypotenus(side_a, side_b):
+        return math.sqrt(math.pow(side_a, 2) + math.pow(side_b, 2))
+
+    # return the current version
+    @staticmethod
+    def current_version():
+        return "Current Version : 1.3.1"
     
-    
+    # return the creator's nickname and his github profile
+    @staticmethod
+    def creator():
+        return "Creator : Tina\nGitHub : https://github.com/Tina-1300"
+
+    # Displays the representation of the Pythagore class
+    @staticmethod
+    def __repr__():
+        return "Pythagoras - Utility classes for the Pythagorean theorem. Created by Tina"
